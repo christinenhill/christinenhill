ab9a9a90bce662b88f2775f6eb0fc38ae2b57411
christinenhill <christine.n.hill@gmail.com>
2015-06-20 19:55:34 +0000

    re order so that it can check the subject

--- includes/send_mail.php
+++ includes/send_mail.php
@@ -6,6 +6,9 @@ if (isset($_POST['name'], $_POST['email'], $_POST['message'])) {
     $email_to = "christine.n.hill@gmail.com";
     $email_from = $_POST['email'];   
     $email_message = $_POST['message']; 
+    
+    # Subject
+    $email_subject = $name . " has sent you an email!";
  
     if ($email_from == ""){
         $error_msg .= "No Sender";
@@ -22,9 +25,6 @@ if (isset($_POST['name'], $_POST['email'], $_POST['message'])) {
         $headers  = 'MIME-Version: 1.0' . "\r\n";
         $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
         $headers .= 'From: <' . $email_from . '>' . "\r\n";
-
-        # Subject
-        $email_subject = $name . " has sent you an email!";
 
         # Send the email
         mail($email_to,$email_subject,nl2br($email_message),$headers);
