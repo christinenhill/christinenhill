13490773a836c13ba82ebf49c5b2820376a4a362
christinenhill <christine.n.hill@gmail.com>
2015-06-20 21:43:49 -0500

    text

--- index.php
+++ index.php
@@ -22,9 +22,8 @@
 		<!-- Header -->
 			<header id="header">
 				<a href="#" class="image avatar"><img src="images/avatar.jpg" alt="" /></a>
-				<h1><strong>I am Strata</strong>, a super simple<br />
-				responsive site template freebie<br />
-				crafted by <a href="http://html5up.net">HTML5 UP</a>.</h1>
+				<h1>Hi, I'm <strong>Christine.</strong><br />
+				Marketing/Design</h1>
 			</header>
 
 		<!-- Main -->
